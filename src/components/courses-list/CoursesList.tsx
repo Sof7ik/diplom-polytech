71933e620921cfc83e55ca76b6100a06f442dbae
Leonid Bychkov <bychkov.l47@mail.ru>
2025-01-09 12:52:29 +0300

    Поправил адаптив страницы списка курсов

--- src/components/courses-list/CoursesList.tsx
+++ src/components/courses-list/CoursesList.tsx
@@ -1,38 +1,49 @@
 import {Link} from "react-router-dom";
 import styles from "./courses-list.module.scss";
 import {courses} from "../../data/courses.ts";
-import {Typography} from "antd";
+import {Form, Input, Typography} from "antd";
 
 export default function CoursesList() {
     return (
-        <ul className={styles.courses__list}>
-            {courses.map((course) => (
-                <li key={course.code}
-                    className={`${styles['course-item__wrapper']}`}>
-                    <div>
-                        <Link to={`/courses/${course.code}/`}
-                              className={styles['course-item__link']}>
+        <>
+            <Form layout="vertical">
+                <Form.Item name="search">
+                    <Input.Search size="large" placeholder="Начните вводить название..." />
+                </Form.Item>
+            </Form>
 
-                            <img src={course.imageSrc}
-                                   alt={course.title}
-                                   className={styles['course-item__image']}/>
+            <ul className={styles.courses__list}>
+                {courses.map((course) => (
+                    <li key={course.code}
+                        className={`${styles['course-item__wrapper']}`}>
+                        <div>
+                            <Link to={`/courses/${course.code}/`}
+                                  className={styles['course-item__link']}>
 
-                            <p className={styles["courseItem__title"]}>
-                                {course.title}
-                            </p>
+                                <img src={course.imageSrc}
+                                       alt={course.title}
+                                       className={styles['course-item__image']}/>
 
-                            <Typography.Text className={styles["courseItem__description"]}>
-                                {course.description}
-                            </Typography.Text>
+                                <p className={styles["courseItem__title"]}>
+                                    {course.title}
+                                </p>
+
+                                <Typography.Text className={styles["courseItem__description"]}>
+                                    {course.description}
+                                </Typography.Text>
+                            </Link>
+                        </div>
+
+                        <Link to={`${course.code}/about/`}
+                                         className={styles["courseItem__about-link"]}>
+                            О курсе
                         </Link>
-                    </div>
+                    </li>
+                ))}
+            </ul>
 
-                    <Link to={`${course.code}/about/`}
-                                     className={styles["courseItem__about-link"]}>
-                        О курсе
-                    </Link>
-                </li>
-            ))}
-        </ul>
+
+        </>
+
     )
 }
